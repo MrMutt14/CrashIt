e5dd17107f980e6815b714e64cc2b4048b9b7840
Matt <MrMutt14@users.noreply.github.com>
2017-03-31 21:38:21 +0300

    Add files via upload

---
+++ CrashIt/CrashIt/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CrashIt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(393, 530);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 183;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Memory";
+            this.columnHeader4.Width = 74;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.openFileLocationToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(174, 70);
+            // 
+            // killToolStripMenuItem
+            // 
+            this.killToolStripMenuItem.Name = "killToolStripMenuItem";
+            this.killToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.killToolStripMenuItem.Text = "Terminate";
+            this.killToolStripMenuItem.Click += new System.EventHandler(this.killToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // openFileLocationToolStripMenuItem
+            // 
+            this.openFileLocationToolStripMenuItem.Name = "openFileLocationToolStripMenuItem";
+            this.openFileLocationToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.openFileLocationToolStripMenuItem.Text = "Open File Location";
+            this.openFileLocationToolStripMenuItem.Click += new System.EventHandler(this.openFileLocationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 532);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Processify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem killToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileLocationToolStripMenuItem;
+    }
+}
+
---
+++ CrashIt/CrashIt/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrashIt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> activeProcesses = new List<int>();
+        List<ListViewItem> itemList = new List<ListViewItem>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Process[] processes = Process.GetProcesses();
+            ListViewItem lstViewItems = null;
+            foreach (Process process in processes)
+            {
+                lstViewItems = new ListViewItem();
+                lstViewItems.Text = process.ProcessName;
+                lstViewItems.SubItems.Add(process.Id.ToString());
+                lstViewItems.SubItems.Add((process.PagedMemorySize64 / 1024 / 1024).ToString() + " MB");
+                listView1.Items.Add(lstViewItems);
+                activeProcesses.Add(process.Id);
+            }
+        }
+        private void RemoveOldProcesses()
+        {
+            List<int> removeMe = new List<int>();
+            foreach (int i in activeProcesses)
+            {
+                // If fails to get PID, remove from active process list
+                try
+                {
+                    Process.GetProcessById(i);
+                }
+                catch (Exception)
+                {
+                    removeMe.Add(i);
+                }
+            }
+            // Separate loops to prevent error
+            foreach (int deadPID in removeMe)
+            {
+                if (activeProcesses.Contains(deadPID))
+                    activeProcesses.Remove(deadPID);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RemoveOldProcesses();
+
+            Process[] processes = Process.GetProcesses();
+            ListViewItem lstViewItems = null;
+            foreach (Process process in processes)
+            {
+                bool exists = false;
+                lstViewItems = new ListViewItem();
+                lstViewItems.Text = process.ProcessName;
+                lstViewItems.SubItems.Add(process.Id.ToString());
+                lstViewItems.SubItems.Add((process.PagedMemorySize64 / 1024 / 1024).ToString() + " MB");
+              
+                foreach (ListViewItem lvi in listView1.Items)
+                {
+                    if (lvi.SubItems[1].Text == process.Id.ToString())
+                    {
+
+                        exists = true;
+                    }
+                    else if (!activeProcesses.Contains(int.Parse(lvi.SubItems[1].Text)))
+                    {
+                        listView1.Items.Remove(lvi);
+                    }
+                }
+                if (exists)
+                    continue;
+                listView1.Items.Add(lstViewItems);
+                activeProcesses.Add(process.Id);
+
+            }
+        }
+
+        private void killToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select a process!", "No process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                foreach (ListViewItem lvi in listView1.SelectedItems)
+                {
+                    Process p = Process.GetProcessById(int.Parse(lvi.SubItems[1].Text));
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Couldn't kill the process\n" + ex.ToString(), "Couldn't kill process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void listView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select a process!", "No process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                foreach (ListViewItem lvi in listView1.SelectedItems)
+                {
+                    Process p = null;
+                    try
+                    {
+                        p = Process.GetProcessById(int.Parse(lvi.SubItems[1].Text));
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    string file = p.MainModule.FileName;
+                    try
+                    {
+                        p.Kill();
+                        Process.Start(file);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Couldn't restart the process\n" + ex.ToString(), "Couldn't restart process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void openFileLocationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select a process!", "No process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                foreach (ListViewItem lvi in listView1.SelectedItems)
+                {
+                    Process p = null;
+                    try
+                    {
+                        p = Process.GetProcessById(int.Parse(lvi.SubItems[1].Text));
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                    string argument = "/select, \"" + p.MainModule.FileName + "\"";
+                    try
+                    {
+                        Process.Start("explorer.exe", argument);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Couldn't restart the process\n" + ex.ToString(), "Couldn't restart process", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
+
